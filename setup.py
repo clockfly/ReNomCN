f198202edf0278929f130bae7c6cdff9ddc9a90c
Yamaguchi Toshihiro <t-yamaguchi@seichogroup.com>
2018-05-14 09:51:15 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+requires = [
+    "numpy", "pandas", "osisoft.pidevclub.piwebapi"
+]
+
+setup(
+    install_requires=requires,
+    name="renom_pi",
+    version="0.0.1",
+    packages=find_packages()
+)
