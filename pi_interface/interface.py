dfa1237bccd15a4b4c0c71631915a33e82e3594c
Yamaguchi Toshihiro <t-yamaguchi@seichogroup.com>
2018-05-17 13:58:09 +0900

    Add setout time for connection.

--- pi_interface/interface.py
+++ pi_interface/interface.py
@@ -24,7 +24,7 @@ class PIInterface(PIWebApiClient):
         self.password = password
         super().__init__(host, False, user, password, False)
 
-    def get_values(self, assetserver, database, elements, attributes, start='*-7d', end='*'):
+    def get_values(self, assetserver, database, elements, attributes, start='*-7d', end='*', request_timeout=5):
         """This method returns values according to given path.
         Args:
             assetserver (str): Asset server name.
@@ -33,14 +33,38 @@ class PIInterface(PIWebApiClient):
             attributes(list): Name list for attributes.
             start(str): Start time.
             end(str): End time.
+            request_timeout(int): Timeout seconds.
         """
         path = 'af:\\\\%s\\%s\\%s|%s' % (assetserver, database, '\\'.join(elements), '|'.join(attributes))
-        response = super().data.get_plot_values(path, None, start, None, None, end, None)
-
+        response = self._get_plot_values(path, None, start, None, None, end, None)
+        print('test')
         df = response.set_index("Timestamp")[["Value"]]
         return df
 
-    def update_values(self, df, assetserver, database, elements, attributes):
+    def _get_plot_values(self, path, desired_units, end_time, intervals, selected_fields, start_time, time_zone, request_timeout=5):
+        if (path is None):
+            print("The variable path cannot be null.")
+
+        web_id = self._convert_path_to_web_id(path, request_timeout=request_timeout)
+        res = super().data.streamApi.get_plot(web_id, desired_units, end_time, intervals, selected_fields, start_time,
+                                      time_zone, _request_timeout=request_timeout)
+        df = super().data.convert_to_df(res.items, selected_fields)
+        return df
+
+    def _convert_path_to_web_id(self, fullPath, request_timeout):
+        system = fullPath[0:3]
+        path = fullPath[3:None]
+        if (system == "af:"):
+            res = super().attribute.get_by_path(path, None, None, _request_timeout=request_timeout)
+            return (res.web_id)
+        elif (system == "pi:"):
+            res = super().attribute.get_by_path(path, None, None, _request_timeout=request_timeout)
+            return (res.web_id)
+        else:
+            print("Error: invalid path. It needs to start with \"pi\" or \"af\"")
+            return
+
+    def update_values(self, df, assetserver, database, elements, attributes, request_timeout=5):
         """This method saves DataFrame to PI AF.
         Args:
             df (pandas.DaraFrame): DataFrame of data to be saved.
@@ -48,6 +72,7 @@ class PIInterface(PIWebApiClient):
             database (str): Database name.
             elements(list): Name list for elements.
             attributes(list): Name list for attributes.
+            request_timeout(int): Timeout seconds.
         """
         path = '\\\\%s\\%s\\%s|%s' % (assetserver, database, '\\'.join(elements), '|'.join(attributes))
         attribute = super().attribute.get_by_path(path, None, None)
