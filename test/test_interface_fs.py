6dd4197dbc79210e6a9ca411dd346c86d89a87cc
takuuuuumi34 <tknm34@gmail.com>
2019-04-08 18:06:50 +0900

    add get fs func

---
+++ test/test_interface_fs.py
@@ -0,0 +1,13 @@
+import pyarrow.filesystem as pafs
+
+from renom_cn.interface import filesystem
+
+
+def test_fs_local():
+    fs = filesystem.get_fs(type="local")
+    assert(isinstance(fs, pafs.LocalFileSystem))
+
+
+def test_fs_s3():
+    fs = filesystem.get_fs(type="s3")
+    assert(isinstance(fs, pafs.S3FSWrapper))
