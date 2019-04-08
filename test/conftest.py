62ee420641f8bc2fdcc44c7bb375246870b29d0d
takuuuuumi34 <tknm34@gmail.com>
2019-04-08 17:40:49 +0900

    add db connector module

---
+++ test/conftest.py
@@ -0,0 +1,20 @@
+import subprocess
+import time
+import pytest
+
+
+def compose_up():
+    subprocess.call(["docker-compose", "up", "-d"])
+    time.sleep(15)
+
+
+def compose_down():
+    subprocess.call(["docker-compose", "down"])
+    subprocess.call(["docker-compose", "rm"])
+
+
+@pytest.fixture(scope='session', autouse=True)
+def scope_class():
+    compose_up()
+    yield
+    compose_down()
---
+++ test/test_db_connector.py
@@ -0,0 +1,137 @@
+from renom_cn.db import connector
+
+from sqlalchemy import Column, Integer, TEXT
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    """ test table """
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(TEXT)
+
+
+def create_table(connection):
+    Base.metadata.create_all(bind=connection.engine)
+
+
+class TestSQLite(object):
+    @classmethod
+    def setup_class(self):
+        self.test_data = {
+            "id": 1,
+            "name": "test"
+        }
+        self.conn = connector.SQLiteConnector()
+        create_table(self.conn)
+
+    @classmethod
+    def teardown_class(self):
+        self.conn.session.close()
+
+    def test_insert(self):
+        d = User(name=self.test_data["name"])
+        d = self.conn.insert(d)
+        assert d.id == self.test_data["id"]
+
+    def test_get_all(self):
+        data_all = self.conn.get_all(User)
+        for d in data_all:
+            assert d.name == self.test_data["name"]
+
+    def test_update(self):
+        update_name = "test_update"
+        data_all = self.conn.get_all(User)
+        for d in data_all:
+            d.name = update_name
+            self.conn.update(d)
+
+        data_all = self.conn.get_all(User)
+        for d in data_all:
+            assert d.name == update_name
+
+
+class TestMySQL(object):
+    @classmethod
+    def setup_class(self):
+        self.test_data = {
+            "id": 1,
+            "name": "test"
+        }
+        host = "localhost"
+        port = "3306"
+        user = "testuser"
+        password = "testpassword"
+        database = "testdb"
+        self.conn = connector.MySQLConnector(host, port, user, password, database)
+        create_table(self.conn)
+
+    @classmethod
+    def teardown_class(self):
+        self.conn.session.close()
+
+    def test_insert(self):
+        d = User(name=self.test_data["name"])
+        d = self.conn.insert(d)
+        assert d.id == self.test_data["id"]
+
+    def test_get_all(self):
+        data_all = self.conn.get_all(User)
+        for d in data_all:
+            assert d.name == self.test_data["name"]
+
+    def test_update(self):
+        update_name = "test_update"
+        data_all = self.conn.get_all(User)
+        for d in data_all:
+            d.name = update_name
+            self.conn.update(d)
+
+        data_all = self.conn.get_all(User)
+        for d in data_all:
+            assert d.name == update_name
+
+
+class TestPostgreSQL(object):
+    @classmethod
+    def setup_class(self):
+        self.test_data = {
+            "id": 1,
+            "name": "test"
+        }
+        host = "localhost"
+        port = "5432"
+        user = "testuser"
+        password = "testpassword"
+        database = "testdb"
+        self.conn = connector.PostgreSQLConnector(host, port, user, password, database)
+        create_table(self.conn)
+
+    @classmethod
+    def teardown_class(self):
+        self.conn.session.close()
+
+    def test_insert(self):
+        d = User(name=self.test_data["name"])
+        d = self.conn.insert(d)
+        assert d.id == self.test_data["id"]
+
+    def test_get_all(self):
+        data_all = self.conn.get_all(User)
+        for d in data_all:
+            assert d.name == self.test_data["name"]
+
+    def test_update(self):
+        update_name = "test_update"
+        data_all = self.conn.get_all(User)
+        for d in data_all:
+            d.name = update_name
+            self.conn.update(d)
+
+        data_all = self.conn.get_all(User)
+        for d in data_all:
+            assert d.name == update_name
